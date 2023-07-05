0c2fd333b315ca3051c0928331cb1065110efdff
Dmitry Mikhaylov <good8815585@yandex.ru>
2023-07-05 19:27:17 +0300

    final

--- homework.py
+++ homework.py
@@ -1,10 +1,16 @@
 import logging
 import os
 import time
+from pathlib import Path
+import sys
 
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
+
+from enum import Enum
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
@@ -24,42 +30,42 @@ HOMEWORK_VERDICTS = {
 }
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG,
-    filename='main.log',
-)
+class MyException(Exception):
+    """My exception."""
 
-#############################################
+    pass
+
+
+class Token(Enum):
+    """Tokens."""
+
+    PRACTICUM_TOKEN = 'PRACTICUM_TOKEN'
+    TELEGRAM_TOKEN = 'TELEGRAM_TOKEN'
+    TELEGRAM_CHAT_ID = 'TELEGRAM_CHAT_ID'
 
 
 def check_tokens():
     """Checking the availability of environment variables."""
-    required_env_vars = [
-        PRACTICUM_TOKEN,
-        TELEGRAM_TOKEN,
-        TELEGRAM_CHAT_ID
-    ]
-    for env_var in required_env_vars:
-        if env_var is None:
-            logging.critical(f'Environment  variable "{env_var}" not found')
-            raise ValueError(f'Environment  variable "{env_var}" not found')
-    return True
+    for token in Token:
+        if globals().get(token.value) is None:
+            raise ValueError(f'Environment variable "{token.value}" not found')
 
 
 def check_response(response: dict) -> list:
     """Checking response."""
     if not isinstance(response, dict):
-        logging.error('Response is not a dictionary')
         raise TypeError('Response does not contain a dict')
 
+    if 'current_date' not in response:
+        raise KeyError('The "current_date" key is not in the answer')
+
     if 'homeworks' not in response:
-        logging.error('The "homeworks" key is not in the answer')
         raise KeyError('The "homeworks" key is not in the answer')
 
     if not isinstance(response['homeworks'], list):
-        logging.error('Response is not a list')
-        raise TypeError('The "homeworks" key does not contain a list')
+        raise TypeError(
+            'The "homeworks" key does not contain a list.',
+            f' Response contein {type(response["homeworks"])}')
 
     return response
 
@@ -73,7 +79,7 @@ def get_api_answer(timestamp):
             }
         )
         if homework_statuses.status_code != 200:
-            raise Exception(
+            raise MyException(
                 f'API request failed:'
                 f' {homework_statuses.status_code}'
             )
@@ -81,8 +87,7 @@ def get_api_answer(timestamp):
         return homework
 
     except requests.exceptions.RequestException as error:
-        logging.error(f'API request failed: {error}')
-        return None
+        raise MyException from error(f'API request failed: {error}')
 
 
 def parse_status(homework: dict) -> str:
@@ -91,13 +96,16 @@ def parse_status(homework: dict) -> str:
         logging.error('No homework_name in dict')
         raise KeyError('No homework name in dict')
 
+    if 'status' not in homework:
+        logging.error('No status in dict')
+        raise KeyError('No status in dict')
     status = homework['status']
 
     if status not in HOMEWORK_VERDICTS:
         logging.error('Status is empty')
         raise KeyError('Status is empty or not in HOMEWORK_VERDICTS')
 
-    verdict = HOMEWORK_VERDICTS.get(status)
+    verdict = HOMEWORK_VERDICTS[status]
     homework_name = homework['homework_name']
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -105,38 +113,54 @@ def parse_status(homework: dict) -> str:
 
 def send_message(bot, message):
     """Send message to Telegramm."""
-    if not TELEGRAM_CHAT_ID:
-        raise ValueError('TELEGRAM_CHAT_ID environment variable is not set')
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.debug('Telegram message has been sent')
     except Exception as error:
-        logging.error(f'Bot failed to send message: {error}')
+        logging.error(f'Bot failed to send message:"{message}", {error}')
 
 
 def main():
     """The main logic of the bot."""
-    if not check_tokens():
-        logging.critical('Environment  variable  not found')
-        exit()
-    else:
-        while True:
-            try:
-                bot = Bot(token=TELEGRAM_TOKEN)
-                timestamp = int(time.time())
-                response = get_api_answer(timestamp)
-                if check_response(response):
-                    if len(response.get('homeworks')) == 0:
-                        logging.debug('No new homework status.')
-                    for homework in response.get('homeworks'):
-                        message = parse_status(homework)
+    last_message_sent = None
+    try:
+        check_tokens()
+    except ValueError as error:
+        logging.critical(f'Environment  variable {error} not found')
+        sys.exit(1)
+    bot = Bot(token=TELEGRAM_TOKEN)
+    timestamp = int(time.time()) - RETRY_PERIOD
+    while True:
+        try:
+            response = get_api_answer(timestamp)
+            check_response(response)
+            if not response['homeworks']:
+                logging.debug('No new homework status.')
+            else:
+                for homework in response.get('homeworks'):
+                    message = parse_status(homework)
+                    if message != last_message_sent:
                         send_message(bot, message)
-            except Exception as error:
-                message = f'Сбой в работе программы: {error}'
-                logging.error(f'Main function faild: {error}')
-            finally:
-                time.sleep(RETRY_PERIOD)
+                        last_message_sent = message
+                timestamp = response['current_date']
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(
+                f'Main function faild: {error}\n',
+                f' type of your response is: {type(response)}'
+            )
+            if message != last_message_sent:
+                send_message(bot, message)
+                last_message_sent = message
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s'
+        + ' - %(message)s - %(funcName)s - %(lineno)d',
+        level=logging.DEBUG,
+        filename=BASE_DIR / 'homework_bot' / 'main.log',
+    )
     main()
